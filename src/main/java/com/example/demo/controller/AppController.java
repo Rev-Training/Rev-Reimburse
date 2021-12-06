3362719fbe5a9d29fb79e752268eda29d636b337
aimenAlt <ymnghost@gmail.com>
2021-12-05 20:28:11 -0800

    Set Spring Boot to send Angular app to front-end

---
+++ src/main/java/com/example/demo/controller/AppController.java
@@ -0,0 +1,21 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api") // optional
+public class AppController {
+    @PostMapping("ox")
+    String ox() {
+        return "I'm Working Ox!";
+    }
+
+}
