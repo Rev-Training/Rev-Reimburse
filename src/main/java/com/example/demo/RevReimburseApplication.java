7dbd9e157bfc2ad38ae8d7aa2a729a7b2f4ce274
aimenAlt <ymnghost@gmail.com>
2021-12-05 19:33:44 -0800

    Add pom.xml plugins.

--- src/main/java/com/example/demo/RevReimburseApplication.java
+++ src/main/java/com/example/demo/RevReimburseApplication.java
@@ -7,7 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RevReimburseApplication {
 
 	public static void main(String[] args) {
+		System.out.println("in");
 		SpringApplication.run(RevReimburseApplication.class, args);
+		System.out.println("out");
+
 	}
 
 }
