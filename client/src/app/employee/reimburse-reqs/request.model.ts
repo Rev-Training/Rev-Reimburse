a172ef910f9aa1c59422e753ec946ac8b47c2a6e
aimenAlt <ymnghost@gmail.com>
2021-12-07 07:54:14 -0800

    Delete client folder contents

--- client/src/app/employee/reimburse-reqs/request.model.ts
+++
@@ -1,9 +0,0 @@
-export class Request {
-  requestID: number = 0;
-  employeeID: number = 0;
-  description: string = '';
-  cost: number = 0.00;
-  date: string = '';
-  status: string = '';
-  imageLink: string = '';
-}
