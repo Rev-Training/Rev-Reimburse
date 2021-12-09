f8285fcd556c75da0773c73b7bc980f8fce3f168
Ps1locyb0rg <dperugini36@gmail.com>
2021-12-09 02:20:58 -0500

    it just works

--- Client/rev-reimburse-angular/src/app/user/user.service.ts
+++ Client/rev-reimburse-angular/src/app/user/user.service.ts
@@ -1,14 +1,26 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { User } from './user.model';
+import { User, UserType } from './user.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  baseURL = "/api/users";
+  userCache: User = {
+    userID: 0,
+    username: '',
+    userPassword: '',
+    firstName: '',
+    lastName: '',
+    dateCreated: '',
+    userEmail: '',
+    userType: UserType.EMPLOYEE,
+    userAddress: ''
+  }
+
+  baseURL = "localhost:7777/api/users";
 
   constructor(private http: HttpClient) { }
 
