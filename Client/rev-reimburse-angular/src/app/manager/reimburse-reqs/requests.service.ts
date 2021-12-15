afbc9bc5e468a5a346d87405a89b9c260e53c3a5
aimenAlt <ymnghost@gmail.com>
2021-12-15 13:46:08 -0800

    Aimen's recent code

--- Client/rev-reimburse-angular/src/app/manager/reimburse-reqs/requests.service.ts
+++ Client/rev-reimburse-angular/src/app/manager/reimburse-reqs/requests.service.ts
@@ -1,28 +1,28 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 import { Observable } from "rxjs";
-import {Request} from "./request.model";
+import {ViewRequest} from "./request.model";
 
 
 @Injectable({
   providedIn: 'root'
 })
-export class RequestService {
+export class ViewRequestService {
 
   baseUrl = "/api/manager-reqs";
 
   constructor(private http: HttpClient) { }
 
-  getAllReqs(): Observable<Request[]>{
-    return this.http.get<Request[]>(this.baseUrl);
+  getAllReqs(): Observable<ViewRequest[]>{
+    return this.http.get<ViewRequest[]>(this.baseUrl);
   }
 
-  getAllEmpReqs(employeeID: number): Observable<Request[]>{
-    return this.http.get<Request[]>(this.baseUrl + "/" + employeeID);
+  getAllEmpReqs(employeeID: number): Observable<ViewRequest[]>{
+    return this.http.get<ViewRequest[]>(this.baseUrl + "/" + employeeID);
   }
 
-  updateRequest(request: Request): Observable<Request>{
-    return this.http.put<Request>(this.baseUrl, request);
+  updateRequest(request: ViewRequest): Observable<ViewRequest>{
+    return this.http.put<ViewRequest>(this.baseUrl, request);
   }
 
 }
